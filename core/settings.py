9a4806a9a19a92e7f575a45b5a62958882cff4e6
Oleg Benets <oleg.benets@web.de>
2025-10-03 18:45:32 +0200

    changed: redis url

--- core/settings.py
+++ core/settings.py
@@ -122,7 +122,7 @@ DATABASES = {
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": os.getenv("REDIS_LOCATION", default="redis://redis:6379/1"),
+        "LOCATION": os.getenv("REDIS_URL", default="redis://redis:6379/1"),
         "OPTIONS": {"CLIENT_CLASS": "django_redis.client.DefaultClient"},
         "KEY_PREFIX": "videoflix",
     }
