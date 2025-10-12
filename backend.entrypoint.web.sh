aaa495cf684982d6a767776b21ec1639bcbda38b
Oleg Benets <oleg.benets@web.de>
2025-10-12 13:37:55 +0200

    changed: entrypoint.sh

--- backend.entrypoint.web.sh
+++ backend.entrypoint.web.sh
@@ -4,15 +4,6 @@ set -e
 
 echo "Warte auf PostgreSQL auf $DB_HOST:$DB_PORT..."
 
-# -q für "quiet" (keine Ausgabe außer Fehlern)
-# Die Schleife läuft, solange pg_isready *nicht* erfolgreich ist (Exit-Code != 0)
-while ! pg_isready -h "$DB_HOST" -p "$DB_PORT" -q; do
-  echo "PostgreSQL ist nicht erreichbar - schlafe 1 Sekunde"
-  sleep 1
-done
-
-echo "PostgreSQL ist bereit - fahre fort..."
-
 # Deine originalen Befehle (ohne wait_for_db)
 python manage.py collectstatic --noinput
 python manage.py makemigrations
